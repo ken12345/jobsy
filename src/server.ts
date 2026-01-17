d6e525506b99ed03b08ad0e2f89c766c31cc7c94
koizzgacilo <koizzgacilo@gmail.com>
2026-01-17 17:24:31 +0800

    koizz exam 1

--- src/server.ts
+++ src/server.ts
@@ -1,20 +1,47 @@
+let arr: number[] = [];
+arr.push(10)
+const num: number = 26;
+arr.push(num);
+arr.push(arr[0] + arr[1]);
+arr.splice(2,0,57);
+arr.push(arr[2] - arr[3]);
+arr.pop();
+arr.shift();
+arr.unshift(46)
+
+import {IStudent} from "./student.interface"
+let studentArr: IStudent[] = [];
+
+function addStudent (student:IStudent){
+    studentArr.push(student);
+};
+
+addStudent({
+    id: 0,
+    name:"Koizz Gacilo",
+    course: "IT",
+    totalGrade: 85
+});
+
+const nameArr: string[] = [
+    "John",
+    "Karen",
+    "Jane",
+    "Mark"
+];
+
+const courseArr: string[] = [
+    "BA",
+    "Criminology",
+    "Secretarial",
+    "Law"
+];
+
+for (let i = 0; i < nameArr.length; i++) {
+    let student: IStudent = {} as IStudent;
 
 
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
+}
 
 
 
@@ -48,6 +75,6 @@
 
 
 //output for arr
-console.log("arr:")
+console.log("arr:", arr, arr.length)
 //output for studentArr
-console.log("studentArr:")
+console.log("studentArr:", studentArr)
