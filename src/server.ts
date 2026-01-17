7c4d43c860722c639eca392d32f48674ee7a4583
ken12345 <kengacilo@gmail.com>
2026-01-17 15:12:14 +0800

    final evaluation

--- src/server.ts
+++ src/server.ts
@@ -1,19 +1,31 @@
 import { IStudent } from './student.interface';
 
-let studentArr: IStudent[] = [];
+let arr: number[] = [];
+
+arr.push(10);
+const num: number = 26;
+arr.push(num);
+arr.push(arr[0] + arr[1])
+arr.splice(2, 0, 57);
+arr.push(arr[2] - arr[3]);
+arr.pop();
+arr.shift();
+arr.unshift(46);
+
+let studentArr: IStudent[] = []; 
 
 let nameArrr: string[] = ["John", "Karen", "Jane", "Mark"];
 let courseArrr: string[] = ["BA", "Criminology", "Secretarial", "Law"];
 const addStudent = (student: IStudent): void => {
     studentArr.push(student);
 }
-addStudent({ id: 0, name: "your name", course: "IT", totalGrade: 85 });
-console.log("Student Array:", studentArr);
-let arr: number[] = [10];
-const num = 26;
+
+addStudent({ id: 0, name: "Novs", course: "IT", totalGrade: 85 });
+
+
 
 for (let i = 0; i < nameArrr.length; i++) {
-    let student: IStudent = {
+    const student: IStudent = {
         id: studentArr.length + 1,
         name: nameArrr[i],
         course: courseArrr[i],
@@ -21,16 +33,9 @@ for (let i = 0; i < nameArrr.length; i++) {
         remarks: arr[i] <= 35 ? "FAILED" : "PASSED"
     };
 
-    addStudent(student);
+  addStudent(student);
 }
 
-// arr.push(num);
-// arr.push(arr[0] + arr[1])
-// arr.splice(2, 0, 57);
-// arr.push(arr[2] - arr[3]);
-// arr.pop();
-// arr.shift();
-// arr.unshift(46);
 
 
 
@@ -50,6 +55,6 @@ for (let i = 0; i < nameArrr.length; i++) {
 
 
 // //output for arr
-// console.log("arr:", arr, arr.length);
+console.log("arr:", arr, arr.length);
 // //output for studentArr
-// console.log("studentArr:", studentArr);
+console.log("studentArr:", studentArr);
