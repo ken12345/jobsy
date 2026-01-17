af2d2986de2a9ee898ec09136d00462621b01fb6
kimainggo <kima.inggo@gmail.com>
2026-01-17 11:36:41 +0800

    kim aloycious

--- src/server.ts
+++ src/server.ts
@@ -1,3 +1,13 @@
+let arr: number[] = [];
+arr.push(10);
+let num: number = 10;
+arr.push(26);
+arr.push(arr[0] + arr[1]);
+arr.splice(2, 0, 57);
+arr.push(arr[2] - arr[3]);
+arr.splice(4, 1);
+arr.splice(0, 1);
+arr.splice(0, 0, 46)
 
 
 
@@ -48,4 +58,6 @@
 
 
 
-console.log("output here:")
+
+
+console.log("output here:", arr)
