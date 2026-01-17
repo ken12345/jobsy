674c6c0bc731bde859510516800249b5b2978ef5
ken12345 <kengacilo@gmail.com>
2026-01-17 11:52:29 +0800

    final evaluation PASS

--- src/server.ts
+++ src/server.ts
@@ -1,7 +1,7 @@
 let arr: number[] = [];
 arr.push(10);
-let num: number = 10;
-arr.push(26);
+let num: number = 26;
+arr.push(num);
 arr.push(arr[0] + arr[1]);
 arr.splice(2, 0, 57);
 arr.push(arr[2] - arr[3]);
@@ -60,4 +60,4 @@ arr.splice(0, 0, 46)
 
 
 
-console.log("output here:", arr)
+console.log("output here:", arr, arr.length)
